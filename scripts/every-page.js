d480606b8f8e10a664b1b2b63aa30aa8102a31fb
Emilia <shaggytocute@gmail.com>
2023-06-11 14:33:09 +0200

    Added reset button

--- scripts/every-page.js
+++ scripts/every-page.js
@@ -21,8 +21,23 @@ function dropDown() {
   }
 }
 
+function resetBox() {
+  const boxElement = document.querySelector(".js-reset-box");
+  if (!boxElement.classList.contains("drop-visible")) {
+    boxElement.classList.add("drop-visible");
+  } else {
+    boxElement.classList.remove("drop-visible");
+  }
+}
+
 document.addEventListener('click', event => {
   if (document.querySelector('.js-gate-down').classList.contains('drop-visible') && event.target.closest('#down-menu') === null) {
     document.querySelector('.js-gate-down').classList.remove('drop-visible');
+  }
+})
+
+document.addEventListener('click', event => {
+  if (document.querySelector('.js-reset-box').classList.contains('drop-visible') && event.target.closest('#reset-box') === null) {
+    document.querySelector('.js-reset-box').classList.remove('drop-visible');
   }
 })
