dae81b57dc830c29691207d83d71d05088270c24
Emilia <shaggytocute@gmail.com>
2023-07-11 16:11:48 +0200

    ciphers download?

--- scripts/cipher-page.js
+++ scripts/cipher-page.js
@@ -3,32 +3,28 @@ let ciphersHTML = "";
 const ciphers = [
   {
     name: "Em cipher",
-    image: "cipher/dietlogo.jpg",
-    rgbColor: "220, 24, 78",
-    keys: getKeys(1, 4),
-    status: getStatus(1, 4),
+    image: "em_cipher.jpg",
   },
 ];
 
-gates.forEach((gate) => {
-  gatesHTML += `
-  <div class="gate-container" style="border-color: rgb(${gate.rgbColor})">
-          <div class="gate-label" style="background-color: rgb(${gate.rgbColor})">
-            ${gate.name}
-          </div>
-          <div class="gate-image-container">
-            <img class="gate-image" src="assets/${gate.image}" />
-          </div>
-          <div class="gate-status-container">
-            <div class="gate-status-text">
-              ${gate.status}
-            </div>
-            <div class="gate-status-locks">
-              ${gate.keys}
-            </div>
-          </div>
-        </div>
+ciphers.forEach((cipher) => {
+  ciphersHTML += `
+  <div class="cipher-container">
+    <div class="cipher-label">
+      ${cipher.name}
+    </div>
+    <div class="cipher-image-container">
+      <a href="https://emiliaahl.github.io/dietgateway/assets/cipher/${cipher.image}">
+        <img class="cipher-image" src="assets/cipher/${cipher.image}" />
+      </a>
+    </div>
+    <a href="https://emiliaahl.github.io/dietgateway/assets/cipher/${cipher.image}" download>
+    <div class="cipher-download">
+      Click to download
+    </div>
+    </a>
+  </div>
   `;
 });
 
-document.querySelector(".js-gate-grid").innerHTML = gatesHTML;
+document.querySelector(".cipher-grid").innerHTML = ciphersHTML;
